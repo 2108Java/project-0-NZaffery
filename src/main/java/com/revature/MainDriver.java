28bc4f29b3cf15cd43cd6ebc42f5105b3ec99be2
unknown <nicholas.zaffery@revature.net>
2021-09-17 11:11:06 -0400

    properties updated

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,10 +1,16 @@
 package com.revature;
 
+
+//banking plan- 2 table,service connection and database connection
+//
+
+
 public class MainDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
